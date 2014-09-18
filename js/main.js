ea783f8102e7a4ba08eb9c8264979875ee5d347f
Paul Davis <clark.victoriajane@gmail.com>
2014-09-18 14:40:00 +1200

    second commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
 //Drop down menu
 
 $(function(){
-	//var bOpen = false;
+	
 
 	$(".nav-handle a").on("click", function(){
 
 		$("#main-nav li").toggleClass("show");
+		
+	});
+	
+	
+
+
+});
+
+//Drop down menu - with locate a squad drop down
+
+$(function(){
+	
+	$(".locate-squad-touch").on("click", function(){
+		
+
+		$("#select").toggleClass("show");
 
 	});
 	
