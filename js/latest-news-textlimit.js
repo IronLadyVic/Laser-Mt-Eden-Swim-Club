1918c6eb5bdd51a15528ec02ce4b9e44943c5a25
IronLadyVic <YOUR@EMAIL.com>
2014-09-29 21:02:26 +1300

    16th commit

--- js/latest-news-textlimit.js
+++ js/latest-news-textlimit.js
@@ -1,4 +1,4 @@
-var maxlength = 120;
+var maxlength = 75;
 $('p#news-contents').text(function (_, text) {
 
     return $.trim(text).substring(0, maxlength);
--- js/main.js
+++ js/main.js
@@ -35,3 +35,23 @@ $(function(){
 $(function() {
     $('.banner').unslider();
 });
+
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+ 
+});
