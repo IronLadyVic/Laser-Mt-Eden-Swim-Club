f60ec8d24b84450125d077ca2ee77d75649b7755
unknown <victoria.clark@44385-WIN-501.auckland.natcoll.ac.nz>
2014-10-12 11:49:16 +1300

    46th commit

---
+++ js/targetIE.js
@@ -0,0 +1,5 @@
+var element = document.documentElement;
+element.setAttribute('data-useragent',  navigator.userAgent);
+element.setAttribute('data-platform', navigator.platform );
+
+// IE 10 == Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)
